43102278c6fe8a82e12c862fe02e8982dd0bb971
George Gilfond <gsgilfond@gmail.com>
2022-07-21 16:37:37 -0400

    implement UI

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {  
     computerSelection = getComputerChoice()
+    /*
     playerSelection = prompt("rock, paper, or scissors?: ")
     playerSelection = playerSelection.toLowerCase()
     while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
@@ -23,38 +24,48 @@ function playRound(playerSelection, computerSelection) {
         playerSelection = prompt("rock, paper, or scissors?: ")
         playerSelection = playerSelection.toLowerCase()
     }
+    */
+
+    const message = document.querySelector('.message')
 
     if (playerSelection == computerSelection) {
-        console.log("It's a tie!")
+        message.innerHTML = "It's a tie!"
         return 0
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You win! Rock beats scissors")
+        message.innerHTML = "You win! Rock beats scissors"
         return 1
     }
     else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You lose! Paper beats rock")
+        message.innerHTML = "You lose! Paper beats rock"
         return -1
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You win! Paper beats rock")
+        message.innerHTML = "You win! Paper beats rock"
         return 1
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log("You lose! Scissors beats paper")
+        message.innerHTML = "You lose! Scissors beats paper"
         return -1
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You win! Scissors beats paper")
+        message.innerHTML = "You win! Scissors beats paper"
         return 1
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("You lose! Rock beats scissors")
+        message.innerHTML = "You lose! Rock beats scissors"
         return -1
     }
 }
 
-function game() {
+function updateScoreboard(playerScore, computerScore) {
+    const playerBox = document.querySelector('.playerBox')
+    const computerBox = document.querySelector('.computerBox')
+    playerBox.innerHTML = playerScore
+    computerBox.innerHTML = computerScore
+}
+
+/* function game() {
     for (let i = 0; i < 5; i++) {
         let decider = playRound()
         if (decider == -1) {
@@ -65,22 +76,52 @@ function game() {
         }
     }
     if (playerScore > computerScore) {
-        console.log("You win the game!")
+        message.innerHTML = ("You win the game!")
     }
     else if (playerScore < computerScore) {
-        console.log("Computer wins the game :(")
+        message.innerHTML = ("Computer wins the game :(")
     }
     else {
-        console.log("The game was tied")
+        message.innerHTML = ("The game was tied")
     }
     return 0
 }
+*/
+
+const end = document.querySelector('.end')
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (playerScore < 5 && computerScore < 5) {
+            let playerSelection = button.id
+            //playRound(playerSelection, computerSelection)
+            let decider = playRound(playerSelection, computerSelection)
+            if (decider == -1) {
+                computerScore++
+            }
+            else if (decider == 1) {
+                playerScore++
+            }
+            updateScoreboard(playerScore, computerScore)
+            if (playerScore >= 5) {
+                end.innerHTML = "Game Over! You Win!"
+            //buttons.forEach((button) => {
+              //  button.removeEventListener('click', )
+            //})
+            }
+            if (computerScore >= 5) {
+                end.innerHTML = "Game Over! You Lose :("
+                //buttons.forEach((button) => {
+                //  button.removeEventListener('click', )
+                // })
+            }
+        }
+    })
+})
 
 let computerSelection = ""
 let playerSelection = ""
 
 let playerScore = 0
 let computerScore = 0
-
-
-game();
